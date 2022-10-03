57deb964818cb642ffb2aae441db7192842e2df7
duongcfa <79086460+duongcfa@users.noreply.github.com>
2022-10-04 02:40:37 +0700

    Update snowstorm.js

--- snowstorm.js
+++ snowstorm.js
@@ -1,16 +1,3 @@
-/** @license
- * DHTML Snowstorm! JavaScript-based snow for web pages
- * Making it snow on the internets since 2003. You're welcome.
- * -----------------------------------------------------------
- * Version 1.44.20131208 (Previous rev: 1.44.20131125)
- * Copyright (c) 2007, Scott Schiller. All rights reserved.
- * Code provided under the BSD License
- * http://schillmania.com/projects/snowstorm/license.txt
- */
-
-/*jslint nomen: true, plusplus: true, sloppy: true, vars: true, white: true */
-/*global window, document, navigator, clearInterval, setInterval */
-
 var snowStorm = (function(window, document) {
 
   // --- common properties ---
@@ -75,12 +62,6 @@ var snowStorm = (function(window, document) {
   features = (function() {
 
     var getAnimationFrame;
-
-    /**
-     * hat tip: paul irish
-     * http://paulirish.com/2011/requestanimationframe-for-smart-animating/
-     * https://gist.github.com/838785
-     */
 
     function timeoutShim(callback) {
       window.setTimeout(callback, 1000/(storm.animationInterval || 20));
